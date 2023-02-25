689b244f0324f827f9a70a45e0d22c67625400ae
Syamantak Pyne <mail2syamantak@gmail.com>
2023-02-25 13:46:21 +0530

    Search

---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import '../App.css';
+
+export default function Search_box() { 
+  return(
+      <div className='container h-full my-1 backdrop-blur-sm bg-white/30 rounded-md px-10 py-3'>
+          <input placeholder='Search the song'id='hash_id'type={'text'} className="w-full col-span-4 h-8 rounded-md bg-white focus:shadow-[12px_12px_19px_3px_#00000024] p-2 focus:outline-0 duration-200"></input>
+      </div>
+    );
+}
