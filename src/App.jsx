ba6e0748a04e1759e33d37815324e25ad2828563
imdebamrita <imdebamrita@gmail.com>
2023-02-25 14:07:15 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,59 @@
-import Upload_box from './components/Upload.jsx';
+
+import { useState } from 'react';
+
+import Upload_box from './Components/Upload.jsx';
+import Search_box from './Components/Search.jsx';
+import Songs_list from './Components/Song_box.jsx';
+import Player from './Components/Player.jsx';
+// import song from './Components/Songs.jsx';
+
+import { init, getList } from './lib/songApi'
+// import bgImg from './assets/darkBackground.png'
+
+const node = await init();
+console.log(node)
+
+const [albumList, tempPlaylist] = await getList(node)
+
 
 
 export default function App() {
+  const [playlist, setPlaylist] = useState(tempPlaylist);
+  const [activeSongIndex, setActiveSongIndex] = useState(-1); // set to -1 for final 
+
   return (
     <div className={ `grid h-screen w-screen place-items-center bg-black text-center px-20 py-5 bg-[url("https://tailwind-ui.hacker5united.repl.co/src/assets/darkBackground.png")]` }>
-      <div className="w-[calc(70vw)] h-full backdrop-blur-md bg-white/30 rounded-md p-2">
-        <Upload_box />
-      </div>
+     <div className='w-[calc(70vw)] h-full mx-auto my-1 backdrop-blur-sm bg-white/30 rounded-md p-2'>
+       <div>
+         <Upload_box 
+		 	node={ node }
+			playlist={ playlist }
+			setPlaylist={ setPlaylist }
+			albumList={ albumList }
+			activeSongIndex={ activeSongIndex }
+			setActiveSongIndex={ setActiveSongIndex }
+		 />
+         <Search_box />
+         <Songs_list 
+			 playlist={ playlist } 
+			 albumList={ albumList }
+			 node={ node }
+			 activeSongIndex={ activeSongIndex }
+			 setActiveSongIndex={ setActiveSongIndex }
+		 />
+         <Player 
+			playlist={ playlist }
+			setPlaylist={ setPlaylist }
+			albumList={ albumList }
+			node={ node }
+			activeSongIndex={ activeSongIndex }
+			setActiveSongIndex={ setActiveSongIndex }
+		 />
+       </div>
+     </div>
     </div>
-  );
+  )
 }
 
+
+// bg-gradient-to-br from-purple-500 to-pink-500
--- src/components/Song_box.jsx
+++ src/components/Song_box.jsx
@@ -1,4 +1,4 @@
-import '../App.css';
+// import '../App.css';
 import React from 'react';
 import Song from './Song.jsx'
 
