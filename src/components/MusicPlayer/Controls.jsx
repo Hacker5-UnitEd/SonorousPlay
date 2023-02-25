bc478efea4a49848168b3d3b65bbf3cd1e91e9b2
Syamantak Pyne <mail2syamantak@gmail.com>
2023-02-25 20:52:01 +0530

    icons and changes

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react';
 
 import leftArrow from '../../assets/leftArrow.svg'
 import rightArrow from '../../assets/rightArrow.svg'
+import pause from '../../assets/pause.svg'
+import play from '../../assets/play.svg'
 
 const Controls = ({ audioRef, isPlaying, setIsPlaying, activeSongIndex, setActiveSongIndex, playlist, audioURI, isLoading }) => {
 	const togglePlayPause = () => {
@@ -69,12 +71,12 @@ const Controls = ({ audioRef, isPlaying, setIsPlaying, activeSongIndex, setActiv
 									// <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   									// 	<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
 									// </svg>
-									<p>{ "Kick" }</p>
+									<img src={ pause } width={ 20 } height={ 20 } />
             				    ) : (
             				        // <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     				// 	<path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
                   					// </svg>
-									  <p>{ "Pl" }</p>
+									  <img src={ play } width={ 20 } height={ 20 } />
 								)
             				}
 						</button>
