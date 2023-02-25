454ffec0d3780010b8b469fd435dd8a06364bf25
shahank42 <crazykavlogs@gmail.com>
2023-02-25 20:06:04 +0530

    Mini-playing animation shows only when not paused

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -42,10 +42,11 @@ export default function Songs_list({ node, songObj, albumArtPath, isActiveSong, 
               
               </div>
 
-              {isActiveSong?
-              <div className='col-span-1 h-6 w-6'>
-                    <img src={ playing } alt="Now Playing..."></img>
-              </div>
+              {
+                (isActiveSong && isPlaying) ?
+                  <div className='col-span-1 h-6 w-6'>
+                        <img src={ playing } alt="Now Playing..."></img>
+                  </div>
                 : 
                 <div className='col-span-1'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:fill-black duration-200">
