dd17646b00cca84a00d645dfbf74ba20ab88d53a
imdebamrita <imdebamrita@gmail.com>
2023-02-25 20:00:05 +0530

    nothing ::

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -60,19 +60,9 @@ export default function Songs_list({ node, songObj, albumArtPath, isActiveSong, 
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
                   </svg>
-                </button>
-
-{/*                 Menu Dropdown */}
-                <div className='group/edit invisible group-hover/item:visible absolute text-xs bg-white p-1 rounded-md border-solid border-slate-600 border z-10 w-24 cursor-pointer'>
-                  <ul>
-                    <li>Add to playlist</li>
-                    <li>Add to playlist</li>
-                    <li>Add to playlist</li>
-                    <li>Add to playlist</li>
-                  </ul>
-                </div>
-                
+                </button>             
               </div>
+              
             </div>
 			    </li>
     );
