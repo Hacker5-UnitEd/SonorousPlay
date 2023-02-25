114ca62a749ae15f73333cdefb83c74c79a25c94
imdebamrita <imdebamrita@gmail.com>
2023-02-25 12:04:55 +0530

    app.jsx frend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-const App = () => {
+export default function App() {
   return (
-    <div className="App">
-      Sonorous
+    <div className={ `grid h-screen w-screen place-items-center bg-black text-center px-20 py-5 bg-[url("https://tailwind-ui.hacker5united.repl.co/src/assets/darkBackground.png")]` }>
+      <div className="w-[calc(70vw)] h-full backdrop-blur-md bg-white/30 rounded-md">sdfsf</div>
     </div>
-  )
+  );
 }
 
-export default App
